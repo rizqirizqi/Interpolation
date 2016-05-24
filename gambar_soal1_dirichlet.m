ddbae18268559f72314c44a8159365c27feef83e
Dwiyan Rahmanianto <rahmanianto@gmail.com>
2016-05-24 21:08:42 +0700

    Creating plot using dirichlet end condition

---
+++ gambar_soal1_dirichlet.m
@@ -0,0 +1,17 @@
+function gambar_soal1_dirichlet(h)
+end_condition = ;
+start_condition = ;
+
+f = @fungsi_soal1;
+x = [-4:h:4];
+max_x = x(length(x));
+for i = 1 : length(x)
+    y(i) = f(x(i));
+end
+xx = [-4:0.01:max_x];
+yy = cubic_spline_custom_end(x, y, xx, end_condition, start_condition);
+
+grid on; hold on;
+plot(x,y,'o',xx,yy);
+title('Cubic Spline Interpolation Soal 1');
+end
